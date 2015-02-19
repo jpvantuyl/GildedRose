dace374f1e78cec62d813325751e09984ce3c118
jpvantuyl <jpvantuyl@gmail.com>
2015-02-19 13:20:06 -0800

    Delete unnecessary cruft from Aged Brie class

--- src/GildedRose.Console/AppreciatingInventory.cs
+++ src/GildedRose.Console/AppreciatingInventory.cs
@@ -14,51 +14,12 @@ namespace GildedRose.Console
 
         public override void Update()
         {
-            if (_item.Name != "Aged Brie" && _item.Name != "Backstage passes to a TAFKAL80ETC concert" && _item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                _item.DecreaseQuality();
-            }
-            else
-            {
-                _item.IncreaseQuality();
+            _item.IncreaseQuality();
 
-                if (_item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (_item.SellIn < 11)
-                    {
-                        _item.IncreaseQuality();
-                    }
-
-                    if (_item.SellIn < 6)
-                    {
-                        _item.IncreaseQuality();
-                    }
-                }
-            }
-
-            if (_item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                _item.DecreaseSellIn();
-            }
+            _item.DecreaseSellIn();
 
             if (_item.SellIn < 0)
-            {
-                if (_item.Name != "Aged Brie")
-                {
-                    if (_item.Name != "Backstage passes to a TAFKAL80ETC concert" && _item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        _item.DecreaseQuality();
-                    }
-                    else
-                    {
-                        _item.Quality = 0;
-                    }
-                }
-                else
-                {
-                    _item.IncreaseQuality();
-                }
-            }
+                _item.IncreaseQuality();
         }
     }
 }

c01c67430ed18a279797008dc5685e1ed7770e10
jpvantuyl <jpvantuyl@gmail.com>
2015-02-19 09:12:59 -0800

    Added a few more boring tests

--- src/GildedRose.Tests/ItemQualityService_UpdateShould.cs
+++ src/GildedRose.Tests/ItemQualityService_UpdateShould.cs
@@ -15,5 +15,35 @@ namespace GildedRose.Tests
 
             Assert.AreEqual(9, item.SellIn);
         }
+
+        [TestMethod]
+        public void LowerQualityByOne()
+        {
+            var item = new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 };
+
+            ItemQualityService.UpdateItemQuality(item);
+
+            Assert.AreEqual(19, item.Quality);
+        }
+
+        [TestMethod]
+        public void LowerQualityByTwoWhenSellInIsZero()
+        {
+            var item = new Item { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 20 };
+
+            ItemQualityService.UpdateItemQuality(item);
+
+            Assert.AreEqual(18, item.Quality);
+        }
+
+        [TestMethod]
+        public void NotLowerQualityBelowZero()
+        {
+            var item = new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0 };
+
+            ItemQualityService.UpdateItemQuality(item);
+
+            Assert.AreEqual(0, item.Quality);
+        }
     }
 }

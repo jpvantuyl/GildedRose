8d08ced32e5049831924e81e2c4f05beaaa3eb06
jpvantuyl <jpvantuyl@gmail.com>
2015-02-19 10:04:27 -0800

    Bounds checking for Backstage Passes

--- src/GildedRose.Tests/ItemQualityService_UpdateShould.cs
+++ src/GildedRose.Tests/ItemQualityService_UpdateShould.cs
@@ -113,6 +113,72 @@ namespace GildedRose.Tests
             Assert.AreEqual(originalSellIn, item.SellIn);
         }
 
+        [TestMethod]
+        public void IncreaseQualityByOneForBackstagePassesAt11Days()
+        {
+            var item = GetBackstagePasses(sellIn: 11);
+            var originalQuality = item.Quality;
+
+            ItemQualityService.UpdateItemQuality(item);
+
+            Assert.AreEqual(originalQuality + 1, item.Quality);
+        }
+
+        [TestMethod]
+        public void IncreaseQualityByTwoForBackstagePassesAt10Days()
+        {
+            var item = GetBackstagePasses(sellIn: 10);
+            var originalQuality = item.Quality;
+
+            ItemQualityService.UpdateItemQuality(item);
+
+            Assert.AreEqual(originalQuality + 2, item.Quality);
+        }
+
+        [TestMethod]
+        public void IncreaseQualityByTwoForBackstagePassesAt6Days()
+        {
+            var item = GetBackstagePasses(sellIn: 6);
+            var originalQuality = item.Quality;
+
+            ItemQualityService.UpdateItemQuality(item);
+
+            Assert.AreEqual(originalQuality + 2, item.Quality);
+        }
+
+        [TestMethod]
+        public void IncreaseQualityByThreeForBackstagePassesAt5Days()
+        {
+            var item = GetBackstagePasses(sellIn: 5);
+            var originalQuality = item.Quality;
+
+            ItemQualityService.UpdateItemQuality(item);
+
+            Assert.AreEqual(originalQuality + 3, item.Quality);
+        }
+
+        [TestMethod]
+        public void IncreaseQualityByThreeForBackstagePassesAt1Day()
+        {
+            var item = GetBackstagePasses(sellIn: 1);
+            var originalQuality = item.Quality;
+
+            ItemQualityService.UpdateItemQuality(item);
+
+            Assert.AreEqual(originalQuality + 3, item.Quality);
+        }
+
+        [TestMethod]
+        public void DropQualityToZeroForBackstagePassesAt0Days()
+        {
+            var item = GetBackstagePasses(sellIn: 0);
+            var originalQuality = item.Quality;
+
+            ItemQualityService.UpdateItemQuality(item);
+
+            Assert.AreEqual(0, item.Quality);
+        }
+
 
         private Item GetBoringItem(int sellIn = 10, int quality = 20)
         {
@@ -127,6 +193,11 @@ namespace GildedRose.Tests
         private Item GetLegendaryItem()
         {
             return new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 };
+        }
+
+        private Item GetBackstagePasses(int sellIn = 10, int quality = 20)
+        {
+            return new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = quality };
         }
     }
 }

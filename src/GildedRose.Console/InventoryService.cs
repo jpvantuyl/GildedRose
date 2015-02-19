0e38a9873426b6ed4aa948cb03e5ac1abdf38f46
jpvantuyl <jpvantuyl@gmail.com>
2015-02-19 13:46:03 -0800

    Added back InventoryService.  Not sure where that file got deleted.

---
+++ src/GildedRose.Console/InventoryService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GildedRose.Console
+{
+    public static class InventoryService
+    {
+        public static void Process(IEnumerable<IInventory> inventoryItems)
+        {
+            foreach (var item in inventoryItems)
+            {
+                Process(item);
+            }
+        }
+
+        public static void Process(IInventory item)
+        {
+            item.Update();
+        }
+    }
+}

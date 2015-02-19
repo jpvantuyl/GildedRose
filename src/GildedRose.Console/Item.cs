92f93a801dee2671dc5b626374638bceb2e89661
jpvantuyl <jpvantuyl@gmail.com>
2015-02-19 08:36:16 -0800

    Tell the Goblin I didn't touch his class

---
+++ src/GildedRose.Console/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GildedRose.Console
+{
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+    }
+}
